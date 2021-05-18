6c5e12e984e15e8967e01c2af1b6110a06b68934
Hubert Dąbrowski <hubsdab@gmail.com>
2021-05-19 01:36:04 +0200

    Raspberry Pi with SesnseHat App for changing LED Display

---
+++ app/build/generated/source/buildConfig/debug/iot/examples/ledcontrol/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package iot.examples.ledcontrol;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "iot.examples.ledcontrol";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
